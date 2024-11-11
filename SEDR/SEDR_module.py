708674fd6dd1465453224c6a9bbff0667b9652bc
toan-ly <toanly0521@gmail.com>
2024-11-10 22:27:06 -0500

    modify sedr without gcn

--- SEDR/SEDR_module.py
+++ SEDR/SEDR_module.py
@@ -322,3 +322,117 @@ class SEDR_impute_module(nn.Module):
         use_adj = adj.clone()
 
         return use_adj, out_x, (mask_nodes, keep_nodes)
+
+class SEDR_no_GCN_module(nn.Module):
+    # Copy SEDR but Replace GCN with linear layer
+    def __init__(
+            self,
+            input_dim,
+            feat_hidden1=64,
+            feat_hidden2=16,
+            linear_hidden=16, # Dimension of the GCN output
+            p_drop=0.2,
+            alpha=1.0,
+            dec_clsuter_n=10,
+    ):
+        super(SEDR_module, self).__init__()
+        self.input_dim = input_dim
+        self.feat_hidden1 = feat_hidden1
+        self.feat_hidden2 = feat_hidden2
+        self.linear_hidden = linear_hidden
+        self.p_drop = p_drop
+        self.alpha = alpha
+        self.dec_cluster_n = dec_clsuter_n
+        self.latent_dim = self.linear_hidden + self.feat_hidden2 # Update latent dimension
+
+        # feature autoencoder
+        self.encoder = nn.Sequential()
+        self.encoder.add_module('encoder_L1', full_block(self.input_dim, self.feat_hidden1, self.p_drop))
+        self.encoder.add_module('encoder_L2', full_block(self.feat_hidden1, self.feat_hidden2, self.p_drop))
+
+        # Linear layer to replace VGAE
+        self.linear_layer = nn.Linear(self.feat_hidden2, self.linear_hidden)
+
+        self.decoder = GraphConvolution(self.latent_dim, self.input_dim, self.p_drop, act=lambda x: x)
+        # self.decoder = nn.Sequential()
+        # self.decoder.add_module('decoder_L0', full_block(self.latent_dim, input_dim, self.p_drop))
+
+        # GCN layers
+        # self.gc1 = GraphConvolution(self.feat_hidden2, self.gcn_hidden1, self.p_drop, act=F.relu)
+        # self.gc2 = GraphConvolution(self.gcn_hidden1, self.gcn_hidden2, self.p_drop, act=lambda x: x)
+        # self.gc3 = GraphConvolution(self.gcn_hidden1, self.gcn_hidden2, self.p_drop, act=lambda x: x)
+        self.dc = InnerProductDecoder(self.p_drop, act=lambda x: x)
+
+        # DEC cluster layer
+        self.cluster_layer = Parameter(torch.Tensor(self.dec_cluster_n, self.gcn_hidden2 + self.feat_hidden2))
+        torch.nn.init.xavier_normal_(self.cluster_layer.data)
+
+        #########################
+        self.enc_mask_token = nn.Parameter(torch.zeros(1, input_dim))
+        self._mask_rate = 0.8
+        self.criterion = self.setup_loss_fn(loss_fn='sce')
+
+    def setup_loss_fn(self, loss_fn, alpha_l=3):
+        if loss_fn == "mse":
+            criterion = nn.MSELoss()
+        elif loss_fn == "sce":
+            criterion = partial(sce_loss, alpha=3)
+        else:
+            raise NotImplementedError
+        return criterion
+
+    def encode(self, x, adj):
+        feat_x = self.encoder(x)
+        # hidden1 = self.gc1(feat_x, adj)
+        linear_output = self.linear_layer(feat_x)
+        return linear_output, feat_x
+
+    # def reparameterize(self, mu, logvar):
+    #     if self.training:
+    #         std = torch.exp(logvar)
+    #         eps = torch.randn_like(std)
+    #         return eps.mul(std).add_(mu)
+    #     else:
+    #         return mu
+
+    def forward(self, x, adj):
+
+        adj, x, (mask_nodes, keep_nodes) = self.encoding_mask_noise(adj, x, self._mask_rate)  #
+
+        linear_output, feat_x = self.encode(x, adj)
+        # gnn_z = self.reparameterize(mu, logvar)
+        z = torch.cat((feat_x, linear_output), 1)
+        de_feat = self.decoder(z, adj)
+
+        # DEC clustering
+        q = 1.0 / (1.0 + torch.sum(torch.pow(z.unsqueeze(1) - self.cluster_layer, 2), 2) / self.alpha)
+        q = q.pow((self.alpha + 1.0) / 2.0)
+        q = (q.t() / torch.sum(q, 1)).t()
+
+        # self-construction loss
+        recon = de_feat.clone()  #
+        x_init = x[mask_nodes]  #
+        x_rec = recon[mask_nodes]  #
+
+        loss = self.criterion(x_rec, x_init)  #
+        # loss = 0
+
+        return z, de_feat, q, feat_x, linear_output, loss
+
+    def encoding_mask_noise(self, adj, x, mask_rate=0.3):
+        num_nodes = adj.shape[0]
+        perm = torch.randperm(num_nodes, device=x.device)
+
+        # random masking
+        num_mask_nodes = int(mask_rate * num_nodes)
+        mask_nodes = perm[: num_mask_nodes]
+        keep_nodes = perm[num_mask_nodes:]
+
+        out_x = x.clone()
+        token_nodes = mask_nodes
+        #out_x[mask_nodes] = 0.0
+
+        out_x[token_nodes] += self.enc_mask_token
+        use_adj = adj.clone()
+
+        return use_adj, out_x, (mask_nodes, keep_nodes)
